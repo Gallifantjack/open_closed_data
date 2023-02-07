487b770d824e7d8d76b48d27baf399168446a739
Gallifantjack <70281273+Gallifantjack@users.noreply.github.com>
2023-02-07 06:55:03 +0000

    final- pre data extract

--- icu_scrape.py
+++ icu_scrape.py
@@ -9,8 +9,8 @@ from tqdm import trange
 
 
 def search(query, retmax, mindate=None, maxdate=None, reldate=None):
-    Entrez.email = "k1927130@kcl.ac.uk"
-    Entrez.api_key = "0e05ceabf7ff6ad2daff4de85eb8ac10360a"
+    Entrez.email = "#######"
+    Entrez.api_key = "#######"
     handle = Entrez.esearch(
         db="pubmed",
         retmax=retmax,
@@ -26,8 +26,8 @@ def search(query, retmax, mindate=None, maxdate=None, reldate=None):
 
 def fetch_details(id_list):
     ids = ",".join(id_list)
-    Entrez.email = "k1927130@kcl.ac.uk"
-    Entrez.api_key = "0e05ceabf7ff6ad2daff4de85eb8ac10360a"
+    Entrez.email = "#######"
+    Entrez.api_key = "#######"
     handle = Entrez.efetch(db="pubmed", retmode="xml", id=ids)
     results = Entrez.read(handle)
     return results
@@ -225,4 +225,4 @@ article_df, search_time, most_recent_article_date = retrieve_articles(
     search_term=search_term, mindate=2010, maxdate=2022, retmax=200000, chunk_size=200
 )
 
-article_df.to_csv("pubmed_icu_articles.csv", index=False)
+article_df.to_csv("data/pubmed_icu_articles.csv", index=False)
